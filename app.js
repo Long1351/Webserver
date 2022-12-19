58d6450006520bb7259184e7a662a0ac430c4a2b
Do Long <kimerly370@gmail.com>
2022-12-19 19:10:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,30 +3,26 @@ const port = 3000;
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if(req.url == '/wordpress/') {
-        fs.readFile('index.php', function(error, data) {
-            res.write(data);
+    if(req.url == '/wp') {
+        res.writeHead(200, { 'Content-Type': 'text/php' });
+        fs.readFile('wordpress/index.php', function(error, data) {
+            if (error) {
+                res.writeHead(404);
+                res.write('Error: File Not Found');
+            } else {
+                res.write(data);
+            }
             res.end();
         });
     }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('index.html', function(error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('Error: File Not Found');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
+});
 
-    server.listen(port, function(error) {
-        if (error) {
-            console.log('Something went wrong', error);
-        } else {
-            console.log('Server is listening on port ' + port);
-        }
-    })
+server.listen(port, function(error) {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log('Server is listening on port ' + port);
+    }
 });
 
 /*
