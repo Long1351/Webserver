5dd32b0d74eb83858524ceacb7767249825b7576
Do Long <kimerly370@gmail.com>
2022-12-25 16:43:00 +0100

    commit

--- express/express.js
+++ express/express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bcrypt = require('bcrypt');
 
 const users = [];
 
@@ -12,11 +13,26 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login.ejs');
 });
+qpp.post('/login', (req, res) => {
+    req.body
+});
 app.get('/register', (req, res) => {
     res.render('register.ejs');
 });
-app.post('/register', (req, res) => {
-    req.body
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+    console.log(users);
 });
 
 app.listen(port, () => {
