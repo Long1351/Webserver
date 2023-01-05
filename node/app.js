60e85f41640c59629297a33c72216797cddc83da
Do Long <kimerly370@gmail.com>
2023-01-05 10:24:20 +0100

    idk

--- node/app.js
+++ node/app.js
@@ -2,32 +2,10 @@ const http = require('http');
 const port = 3000;
 const fs = require('fs'); 
 
-const server = http.createServer((req, res) => {
-    if (req.url == '/addItem') {
-        let body;
-        req.on('data', chunk => {
-            body += chunk;
-        });
-        req.on('end', () => {
-            
-        });
-
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World');
-    }
-    
-
-    if (req.url != '/') { 
-        if (fs.existsSync('ui' + req.url)) {
-            res.writeHead(200, {});
-            fs.readFile('ui' + req.url,(error, data) => {
-                res.write(data);
-                res.end();
-            });
-        }
-    } else {
+const server = http.createServer((req, res) => {  
+    if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.readFile('node/ui/index.html', (error, data) => {
+        fs.readFile('node/ui/index.html', function(error, data) {
             if (error) {
                 console.error(error);
                 res.end('An error occurred');
@@ -39,10 +17,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(port, (error) => {
-    if (error) {
-        console.log('Something went wrong', error);
-        return;
-    }
+server.listen(port, () => {
     console.log('Server is listening on port ' + port);
 });
