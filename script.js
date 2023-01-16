f643365a64ed29e5dcdc6442d162438c85556f43
Do Long <kimerly370@gmail.com>
2023-01-16 19:20:54 +0100

    timo ist coool

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const text = document.querySelector(".sec-text");
-const textLoad = () => {
+const fs = require("fs");
+const text = document.getElementsByClassName("sec-text")[0];
+function textLoad() {
     setTimeout(() => {
         text.textContent = "Do Long";
-        text.style.animation = "animate 6s steps(25) infinite;";
+        
     }, 0);
-    setTimeout(() => { text.textContent = "ein Discord Bot Developer"; }, 6000);
-    setTimeout(() => { text.textContent = "ein Web-Entwickler"; }, 12000);
+    setTimeout(() => { text.textContent = "ein Discord Bot Developer"; }, 3000);
+    setTimeout(() => { text.textContent = "ein Web-Entwickler"; }, 6000);
 }
 textLoad();
-setInterval(textLoad, 18000);
+setInterval(textLoad, 9000);
