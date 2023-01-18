5285678a5130dddec16c59955b3f5a55b1b833ca
Do Long <kimerly370@gmail.com>
2023-01-18 16:23:19 +0100

    Animation Bug Fix

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const fs = require("fs");
-const text = document.getElementsByClassName("sec-text")[0];
+const text = document.getElementById("sec-text");
 function textLoad() {
     setTimeout(() => {
         text.textContent = "Do Long";
-        
     }, 0);
-    setTimeout(() => { text.textContent = "ein Discord Bot Developer"; }, 3000);
-    setTimeout(() => { text.textContent = "ein Web-Entwickler"; }, 6000);
+    setTimeout(() => {
+        text.textContent = "ein Discord Bot Developer";
+    }, 3000);
+    setTimeout(() => {
+        text.textContent = "ein Web-Entwickler";
+    }, 6000);
 }
 textLoad();
 setInterval(textLoad, 9000);
