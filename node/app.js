da3df0bc0a19e0de64b2806f1caafd97ceacc435
Do Long <kimerly370@gmail.com>
2023-01-21 16:09:31 +0100

    idk what this is but it's probably important

--- node/app.js
+++ node/app.js
@@ -1,18 +1,31 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 const port = 1000;
 
 http.createServer((req, res) => {
-    if (req.url == '/') {
-        fs.readFile('./node/ui/index.html', (err, data) => {
-            if (err) {
-                res.writeHead(404);
-                res.write('Error: File Not Found');
+    function sendFile(filename, contentType) {
+        fs.readFile(filename, (error, data) => {
+            if (error) {
+                console.error(error);
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.write("Something does not work well");
             } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.writeHead(200, { "Content-Type": contentType });
                 res.write(data);
             }
-            res.end();
-        })
-    };
-}).listen(port, () => console.log(`Server running on port ${port}`));
+        });
+    }
+    
+    if (req.url == "/") {
+        sendFile("./node/ui/index.html", "text/html");
+        sendFile("./node/ui/style.css", "text/css");
+        sendFile("./node/ui/script.js", "text/javascript");
+        res.end();
+    }
+}).listen(port, (error) => {
+    if (error) {
+        console.log("Something went wrong" + error);
+    } else {
+        console.log("Everything is working fine on port " + port);
+    }
+});
